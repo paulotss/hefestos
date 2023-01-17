7e4bac83ad4ae6470620976577b8bf21c57ab4a9
Paulo de Tarso <paulo.oinab@gmail.com>
2023-01-17 19:57:35 -0300

    Create migration create-products-table

---
+++ src/database/migrations/20230117225157-create-products-table.js
@@ -0,0 +1,47 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('products', {
+      id: {
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      description: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      amount: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      width: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      height: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      depth: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      weight: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('products');
+  }
+};
