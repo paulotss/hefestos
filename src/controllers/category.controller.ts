fcc6a58d4f5518572fdd1c12107ea29a694f0085
Paulo de Tarso <paulo.oinab@gmail.com>
2023-03-20 15:28:14 -0300

    Cria método getAll para categories

---
+++ src/controllers/category.controller.ts
@@ -0,0 +1,27 @@
+import { Request, Response, NextFunction } from 'express';
+import CategoryService from '../services/category.service';
+
+class CategoryController {
+  private request: Request;
+  private resonse: Response;
+  private next: NextFunction;
+  private service: CategoryService;
+  
+  constructor(req: Request, res: Response, next: NextFunction) {
+    this.request = req;
+    this.resonse = res;
+    this.next = next;
+    this.service = new CategoryService();
+  }
+
+  public async getAll() {
+    try {
+      const result = await this.service.getAll();
+      this.resonse.status(200).json(result);
+    } catch (error) {
+      this.next(error);
+    }
+  }
+}
+
+export default CategoryController;
