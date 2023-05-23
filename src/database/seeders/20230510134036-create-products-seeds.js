3870c860092357ca64979c4dc99bf14b838fdc9a
Paulo de Tarso <paulo.oinab@gmail.com>
2023-05-23 14:28:42 -0300

    change amount for 10 in products seed

--- src/database/seeders/20230510134036-create-products-seeds.js
+++ src/database/seeders/20230510134036-create-products-seeds.js
@@ -9,6 +9,7 @@ module.exports = {
         description: "2023, Fundição a frio, resina com pó de ferro, 10 x 4 x 4 cm",
         cover: "1680620497916.jpg",
         price: 25,
+        amount: 10,
         user_id: 1,
         category_id: 1
       },
@@ -17,6 +18,7 @@ module.exports = {
         description: "2023, Fundição a frio com pó de ferro e imitação de folha de prata e base de mármore, 25 x 32 x 16cm",
         cover: "1680620102815.jpg",
         price: 560,
+        amount: 10,
         user_id: 1,
         category_id: 1
       },
@@ -25,6 +27,7 @@ module.exports = {
         description: "2023, Fundição a frio , resina com pó de mármore, 20 x 10 x 10 cm",
         cover: "1680620260682.jpg",
         price: 70,
+        amount: 10,
         user_id: 1,
         category_id: 1
       },
@@ -33,6 +36,7 @@ module.exports = {
         description: "2023, Fundição a frio , resina com pó de bronze, 14x 10 x 10cm",
         cover: "1680620339196.jpg",
         price: 70,
+        amount: 10,
         user_id: 1,
         category_id: 1
       },
